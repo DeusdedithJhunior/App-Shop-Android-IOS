6c6057dd427574774623e04f9d220f6ac95f6bfa
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-22 16:15:47 -0300

    creating the first named navigation

--- AppShop/shop/lib/components/products_item.dart
+++ AppShop/shop/lib/components/products_item.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/utils/app_routes.dart';
+
+// aqui vai ficar as caracteristicas de como os itens irão ser
+// exibos em products overview page
 
 class ProductItem extends StatelessWidget {
   final Product product;
@@ -12,6 +16,38 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(product.title);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        // ignore: sort_child_properties_last
+        child: GestureDetector(
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(AppRoutes.routeProductDetail, arguments: product);
+          },
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black45,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.favorite),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+      ),
+    );
   }
 }
