94f1fa2b40408d4de413f0b229e50642e7206d1b
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-22 16:43:28 -0300

    using ModalRoute

--- AppShop/shop/lib/pages/product_detail_page.dart
+++ AppShop/shop/lib/pages/product_detail_page.dart
@@ -4,16 +4,16 @@ import 'package:shop/models/product.dart';
 // aqui vai ficar a classe que irá mostrar os detalhes dos produtos
 class ProductDetailPage extends StatelessWidget {
   // produto que será passado
-  final Product product;
 
   // construtor
   const ProductDetailPage({
     Key? key,
-    required this.product,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Product product =
+        ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
