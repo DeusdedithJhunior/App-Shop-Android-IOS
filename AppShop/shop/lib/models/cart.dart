3e9ccc47807c6aeeff8d50325a0a54e84d3fb6e8
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-23 15:36:52 -0300

    creating cart class methods

--- AppShop/shop/lib/models/cart.dart
+++ AppShop/shop/lib/models/cart.dart
@@ -1,11 +1,18 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:shop/models/cart_item.dart';
+import 'package:shop/models/product.dart';
 
 class Cart with ChangeNotifier {
   Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items {
     return {..._items};
+  }
+
+  // metodo que informa quantos itens tem no carrinho
+  int get itemsCount {
+    return _items.length;
   }
 
   // metodo que remove os itens do carrinho
@@ -18,5 +25,42 @@ class Cart with ChangeNotifier {
   void clear() {
     _items = {};
     notifyListeners();
+  }
+
+  // metodo que adiciona itens no carrinho
+  void addItem(Product product) {
+    if (_items.containsKey(product.id)) {
+      _items.update(
+        product.id,
+        (exitingItem) => CartItem(
+          id: exitingItem.id,
+          name: exitingItem.name,
+          productId: exitingItem.productId,
+          quantity: exitingItem.quantity + 1,
+          price: exitingItem.price,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        product.id,
+        () => CartItem(
+          id: Random().nextDouble().toString(),
+          name: product.title,
+          productId: product.id,
+          quantity: 1,
+          price: product.price,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+
+  // metodo que calcula o total
+  double get totalAmount {
+    double total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
   }
 }
