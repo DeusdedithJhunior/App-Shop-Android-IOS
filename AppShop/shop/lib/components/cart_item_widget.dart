edf702798f17daa398afe7eac45b8bbb21d741ee
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-24 20:16:31 -0300

    onDismissed active

--- AppShop/shop/lib/components/cart_item_widget.dart
+++ AppShop/shop/lib/components/cart_item_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 import 'package:shop/models/cart_item.dart';
 
 // componente visual da tela carrinho
@@ -10,24 +12,52 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Theme.of(context).errorColor,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
+      onDismissed: (_) {
+        Provider.of<Cart>(
+          context,
+          listen: false,
+        ).removeItem(cartItem.productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
         child: Padding(
-          padding: const EdgeInsets.all(5),
-          child: FittedBox(
-            child: Text(
-              '${cartItem.price}',
-              style: TextStyle(
-                color: Theme.of(context).primaryTextTheme.headline6?.color,
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text(
+                    '${cartItem.price}',
+                    style: TextStyle(
+                      color:
+                          Theme.of(context).primaryTextTheme.headline6?.color,
+                    ),
+                  ),
+                ),
               ),
             ),
+            title: Text(cartItem.name),
+            subtitle: Text('Total: R\$ ${cartItem.price * cartItem.price}'),
+            trailing: Text('${cartItem.quantity}x'),
           ),
         ),
       ),
-      title: Text(cartItem.name),
-      subtitle: Text('Total: R\$ ${cartItem.price * cartItem.price}'),
-      trailing: Text('${cartItem.quantity}x'),
     );
   }
 }
