8d44a6f0caeb6f114fc052aa5a9f19de3ad65051
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-02 12:34:43 -0300

    creating routes

--- AppShop/shop/lib/utils/app_routes.dart
+++ AppShop/shop/lib/utils/app_routes.dart
@@ -1,4 +1,6 @@
 class AppRoutes {
+  static const routeHome = "/";
   static const routeProductDetail = "/product-detail";
   static const cartPage = "/cart-page";
+  static const routeOrders = "/orders";
 }
