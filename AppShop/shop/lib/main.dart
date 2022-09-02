bd0e0499520328d925c348937158cb30437f8c77
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-02 16:03:33 -0300

    adding drawer

--- AppShop/shop/lib/main.dart
+++ AppShop/shop/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:shop/models/cart.dart';
 import 'package:shop/models/order_list.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/cart_page.dart';
+import 'package:shop/pages/orders_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'pages/products_overview_page.dart';
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider( //aqui os dados são compartilhados por todo app
+        ChangeNotifierProvider(
+          //aqui os dados são compartilhados por todo app
           create: (_) => ProductList(), // criação de nova lista de produtos,
         ),
         ChangeNotifierProvider(
@@ -38,10 +40,12 @@ class MyApp extends StatelessWidget {
           ).copyWith(secondary: Colors.deepOrange),
           fontFamily: 'Lato',
         ),
-        home: const ProductsOverviewPage(),
+        //home: const ProductsOverviewPage(),
         routes: {
+          AppRoutes.routeHome: (ctx) => const ProductsOverviewPage(),
           AppRoutes.routeProductDetail: (ctx) => const ProductDetailPage(),
-          AppRoutes.cartPage: (ctx) => const CartPage()
+          AppRoutes.cartPage: (ctx) => const CartPage(),
+          AppRoutes.routeOrders: (ctx) => const OrdersPage(),
         },
         debugShowCheckedModeBanner: false,
       ),
