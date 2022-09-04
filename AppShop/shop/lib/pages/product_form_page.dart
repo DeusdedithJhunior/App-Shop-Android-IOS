7fea176406955af9d3ea053aa25544df9ae346a8
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-04 00:00:55 -0300

    method dispose

--- AppShop/shop/lib/pages/product_form_page.dart
+++ AppShop/shop/lib/pages/product_form_page.dart
@@ -8,11 +8,55 @@ class ProductFormPage extends StatefulWidget {
 }
 
 class _ProductFormPageState extends State<ProductFormPage> {
+  final _priceFocus = FocusNode();
+  final _descriptionFocus = FocusNode();
+
+  // libera recursos caso a tela seja liberada também
+  @override
+  void dispose() {
+    super.dispose();
+    _priceFocus.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Formulário de Produtos'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              TextField(
+                decoration: const InputDecoration(labelText: 'Nome'),
+                textInputAction: TextInputAction.next,
+                onSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocus);
+                },
+              ),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Preço'),
+                textInputAction: TextInputAction.next,
+                focusNode: _priceFocus,
+                onSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocus);
+                },
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Descrição'),
+                textInputAction: TextInputAction.next,
+                focusNode: _descriptionFocus,
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
